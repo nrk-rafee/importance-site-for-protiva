9ea5080385b179b49b8cf511c7a3c764c405ffb9
nrk-rafee <ahrafee3@gmail.com>
2026-08-17 14:31:10 +0600

    Update LoginScreen.jsx

--- src/components/screens/login/LoginScreen.jsx
+++ src/components/screens/login/LoginScreen.jsx
@@ -1,1 +1,23 @@
+import React from "react";
 
+function LoginScreen() {
+  return (
+    <div>
+      <h1>Login Page</h1>
+
+      <input type="text" placeholder="Username" />
+
+      <br />
+      <br />
+
+      <input type="password" placeholder="Password" />
+
+      <br />
+      <br />
+
+      <button>Login</button>
+    </div>
+  );
+}
+
+export default LoginScreen;
