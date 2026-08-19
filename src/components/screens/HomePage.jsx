74fb3a8d44e37fd9f8c82551c2899f68f7206808
nrk-rafee <ahrafee3@gmail.com>
2026-08-19 20:06:08 +0600

    Update HomePage.jsx

--- src/components/screens/HomePage.jsx
+++ src/components/screens/HomePage.jsx
@@ -1,1 +1,15 @@
+import React from "react";
 
+function HomePage() {
+  return (
+    <div>
+      <h1>Welcome madam  ❤️</h1>
+
+      <p>
+        You have successfully logged in.
+      </p>
+    </div>
+  );
+}
+
+export default HomePage;
